a50ab7a245510d9741ffa5b74e305c7cfe68138c
olioni <o.taumoepeau@taiwananga.co.nz>
2022-03-15 17:59:47 +1300

    added parallax background

---
+++ assets/images/Other/Terrain/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="terrain_tiles" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="terrain_tiles.png" width="256" height="256"/>
+</tileset>
--- game.js
+++ game.js
@@ -20,35 +20,32 @@ var config = {
 
 const game = new Phaser.Game(config);
 
-var player = ''
-var gameOver = false
-var backgroundImage = ''
-var timedEvent = ''
+let player = ''
+let backgroundImage = ''
 
 var spawnX = 50
 var spawnY = 400
 
-var doubleJump = false
-var jumps = 0
-
-var health = 100
-
 function preload() {
-    // background
-    this.load.image('background', 'assets/images/Backgrounds/game-background.png')
-    
-    // map
-    this.load.tilemapTiledJSON('map', 'assets/map/test.json')
-    this.load.image('groundsImg', 'assets/spritesheets/spritesheet_ground.png');
-    this.load.image('tilesImg', 'assets/spritesheets/spritesheet_tiles.png');
-    
-    // player
-    // this.load.atlasXML('alien', 'assets/spritesheets/spritesheet_players.png', 'assets/spritesheets/spritesheet_players.xml');
+    // preload background layers
+    this.load.image('under-layer', 'assets/images/Backgrounds/TreeLine/under-layer.png')
+    this.load.image('back-row', 'assets/images/Backgrounds/TreeLine/back-row.png')
+    this.load.image('middle-row', 'assets/images/Backgrounds/TreeLine/middle-row.png')
+    this.load.image('front-row', 'assets/images/Backgrounds/TreeLine/front-row.png')
+
+    // preload tiledmap json file, ground textures, tile textures, item textures
+    this.load.tilemapTiledJSON('map', 'assets/map/untitled.json')
+    this.load.image('groundsImg', 'assets/spritesheets/spritesheet_ground.png')
+    this.load.image('tilesImg', 'assets/spritesheets/spritesheet_tiles.png')
+    this.load.image('itemsImg', 'assets/spritesheets/spritesheet_items.png')
+
+    // preload player spritesheet
     this.load.spritesheet('taneIdle', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Ftane-idle.png?v=1606611069685', {
         frameWidth: 128,
         frameHeight: 128
       }
     );
+
     this.load.spritesheet('taneJump',
       'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Ftane-jump.png?v=1606611070167', {
         frameWidth: 128,
@@ -62,243 +59,187 @@ function preload() {
         frameHeight: 128
       }
     );
+
     this.load.spritesheet('taneAttack',
       'https://cdn.glitch.com/5095b2d7-4d22-4866-a3b8-5f744eb40eb0%2F128-Attack%20Sprite.png?v=1602576237547', {
         frameWidth: 128,
         frameHeight: 128
       }
     );
+
     this.load.spritesheet('taneDeath',
       'https://cdn.glitch.com/5095b2d7-4d22-4866-a3b8-5f744eb40eb0%2F128-Death-Sprite.png?v=1602576237169', {
         frameWidth: 128,
         frameHeight: 128
       }
     );
-    
-    // enemy
-    this.load.atlasXML('enemies', 'assets/spritesheets/spritesheet_enemies.png', 'assets/spritesheets/spritesheet_enemies.xml');
-    this.load.image('bee', 'assets/images/Enemies/bee.png')
-    this.load.image('fish', 'assets/images/Enemies/fishBlue.png')
-
 }
 
 function create() {
-    /* Create and define player, enemies, items and map */
-    // background
-    backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
-    backgroundImage.setScale(1)
-    backgroundImage.setScrollFactor(0.1)
+    // create and display background image
+    layer1 = this.add.image(0, 0, 'under-layer').setOrigin(0, 0)
+    layer2 = this.add.image(0, 0, 'back-row').setOrigin(0, 0)
+    layer3 = this.add.image(0, 0, 'middle-row').setOrigin(0, 0)
+    layer4 = this.add.image(0, 0, 'front-row').setOrigin(0, 0)
     
-    // map
-    const map = this.make.tilemap({key: 'map' });
+    layer1.setScale(0.6)
+    layer1.setScrollFactor(0.25)
 
-    // ground
-    const groundsTileset = map.addTilesetImage('spritesheet_ground', 'groundsImg');
-    const tilesTileset = map.addTilesetImage('spritesheet_tiles', 'tilesImg')
+    layer2.setScale(0.6)
+    layer2.setScrollFactor(0.2)
+
+    layer3.setScale(0.6)
+    layer3.setScrollFactor(0.15)
+
+    layer4.setScale(0.6)
+    layer4.setScrollFactor(0.1)
+
+    // create and define the map for the player
+    const map = this.make.tilemap({key: 'map'})
     
-    const platforms = map.createStaticLayer('Platforms', groundsTileset, 0, 0);
-    const tiles = map.createStaticLayer('Tiles', tilesTileset, 0, 0)
+    // add the ground image to the tileset of the map
+    const groundTileset = map.addTilesetImage('spritesheet_ground', 'groundsImg')
+    // const tileTileset = map.addTilesetImage('spritesheet_tiles', 'tilesImg')
+    // const itemTileset = map.addTilesetImage('spritesheet_items', 'itemsImg')
+
+    // create and define each layer in the json file as its on layer
+    const platforms = map.createLayer('Platforms', groundTileset, 0, 0)
+    // const items = map.createLayer('Coins', itemTileset, 0, 0)
+    // const crates = map.createLayer('Crates', tileTileset, 0, 0)
+    // const bridges = map.createLayer('Bridges', tileTileset, 0, 0)
+    // const liquids = map.createLayer('Liquids', tileTileset, 0, 0)
+    // const spikes = map.createLayer('Spikes', tileTileset, 0, 0)
 
     platforms.setScale(0.3)
-    platforms.setCollisionByExclusion(-1, true);
+    platforms.setCollisionByExclusion(-1, true)
 
-    tiles.setScale(0.3)
-    tiles.setCollisionByExclusion(-1, true)
+    // crates.setScale(0.3)
+    // crates.setCollisionByExclusion(-1, true)
 
-    // player
-    player = this.physics.add.sprite(50, 400, 'taneIdle');
-    player.setScale(1)
-    player.setBounce(0.05);
-    player.body.setSize(player.width - 100, player.height - 50).setOffset(50, 25);
-    
-    // collider
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(player, tiles)
+    // items.setScale(0.3)
+    // items.setCollisionByExclusion(-1, true)
 
-    /* Create and define a camera to follow the player */
-    // --- Camera ---
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, 570)
+    // bridges.setScale(0.3)
+    // bridges.setCollisionByExclusion(-1, true)
+
+    // liquids.setScale(0.3)
+    // liquids.setCollisionByExclusion(-1, true)
+
+    // spikes.setScale(0.3)
+    // spikes.setCollisionByExclusion(-1, true)
+
+    // create and define the moveable player
+    player = this.physics.add.sprite(50, 400, 'taneIdle')
+    player.setScale(0.8)
+    player.setBounce(0.2)
+    player.body.setSize(player.width - 100, player.height - 50).setOffset(50, 25)
+
+    // add collisions between player and the map
+    this.physics.add.collider(player, platforms)
+    // this.physics.add.collider(player, crates)
+    // this.physics.add.collider(player, bridges)
+
+    // this.physics.add.collider(player, spikes, function(player, spikes){console.log('player touched spikes')})
+
+    // define the keyboard keys
+    cursors = this.input.keyboard.createCursorKeys()
+
+    // create and define a camera
     var camera = this.cameras.main
+    camera.setBounds(0, 0, 5765, 1140)
     camera.startFollow(player, true)
-    camera.setZoom(1)
+    camera.setZoom(1.2)
 
-    // --- Controls ---
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    // --- Camera config ---
+    // camera config
     const controlConfig = {
         camera: camera,
-        left: this.cursors.left,
-        right: this.cursors.right,
-        up: this.cursors.up,
-        down: this.cursors.down,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
         acceleration: 0.01,
         drag: 0.0005,
-        maxSpeed: 0.2,
+        maxSpeed: 0.2
     }
 
-    this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-    
-    // animations
-    this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNames('taneRun', {
-            frames: [16, 17, 18, 19, 20, 21, 22, 23]
-        }),
-        frameRate: 10,
-        repeat: -1
-    })
+    this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig)
 
+    // player walking animation
     this.anims.create({
-        key: 'idle',
-        frames: this.anims.generateFrameNumbers('taneIdle', {
-          frames: [6, 7, 8]
-        }),
-        frameRate: 5,
-        repeat: -1
-      });
-  
+      key: 'walk',
+      frames: this.anims.generateFrameNames('taneRun', {
+        frames: [16, 17, 18, 19, 20, 21, 22, 23]
+      }),
+      frameRate: 10,
+      repeat: -1
+    })
     this.anims.create({
-        key: 'jump',
-        frames: this.anims.generateFrameNumbers('taneJump', {
-            frames: [13, 15]
-        }),
-        frameRate: 10,
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('taneIdle', {
+        frames: [6, 7, 8]
+      }),
+      frameRate: 5,
+      repeat: -1
     });
 
     this.anims.create({
-        key: 'attack',
-        frames: this.anims.generateFrameNumbers('taneAttack', {
-            frames: [8, 9, 10, 11]
-        }),
-        frameRate: 10,
+      key: 'jump',
+      frames: this.anims.generateFrameNumbers('taneJump', {
+        frames: [13, 15]
+      }),
+      frameRate: 10,
     });
 
     this.anims.create({
-        key: 'die',
-        frames: this.anims.generateFrameNumbers('taneDeath', {
-            frames: [1, 2, 3, 4, 5]
-        }),
-        frameRate: 10,
-    })
-
-    // enemies
-    this.anims.create({
-        key: 'beeMove',
-        frames: [{ key: 'enemies', frame: 'bee.png'},{ key: 'enemies', frame: 'bee_move.png'} ],
-        frameRate: 10,
-        repeat: -1
+      key: 'attack',
+      frames: this.anims.generateFrameNumbers('taneAttack', {
+          frames: [8, 9, 10, 11]
+      }),
+      frameRate: 10,
     });
+
     this.anims.create({
-        key: 'fishUp',
-        frames: [{key: 'enemies', frame: 'fishBlue.png'}]
+      key: 'die',
+      frames: this.anims.generateFrameNumbers('taneDeath', {
+          frames: [1, 2, 3, 4, 5]
+      }),
+      frameRate: 10,
     })
-    
-    this.enemies = this.physics.add.group({allowGravity: false,immovable: true});
-    const enemyObj = map.getObjectLayer('Enemies')['objects'];
-    enemyObj.forEach(enemyObj => {
-        if (enemyObj.type == 'bee') {
-            const bee = this.enemies.create(enemyObj.x * 0.3, (enemyObj.y * 0.3) - enemyObj.height, 'bee').setOrigin(0, 0).setScale(0.3)
-            this.tweens.add({
-                targets: bee,
-                y: 200,
-                duration: 2000,
-                ease: 'Sine.easeInOut',
-                repeat: -1,
-                yoyo: true
-            });
-            bee.play('beeMove', true);
-        }
-        if (enemyObj.type == 'fish') {
-            const fish = this.enemies.create(enemyObj.x * 0.3, (enemyObj.y * 0.4) - enemyObj.height, 'fish').setOrigin(0, 0).setScale(0.3)
-            this.tweens.add({
-                targets: fish,
-                y: 200,
-                duration: 1000,
-                ease: 'Sine.easeInOut',
-                repeat: -1,
-                yoyo: true
-            });
-            fish.play('fishUp', true);
-        }
-    })
-    this.physics.add.overlap(player, this.enemies, hitEnemy, null, this);
 }
 
 function update(time, delta) {
-    if (gameOver === true) {
-        return;
-    }
+  player.setFlipX(true)
+  // define player walking speed
+  const speedX = 300
+  const speedY = -620
 
-    this.controls.update(delta);
+  this.controls.update(delta)
 
-    const speed = 500
-    // Control the player with left or right keys
-    // if (touchedBee == false) {
-        if (this.cursors.left.isDown) {
-            player.setVelocityX(-speed);
-            if (player.body.onFloor()) {
-            player.play('walk', true);
-            }
-        } else if (this.cursors.right.isDown) {
-            player.setVelocityX(speed);
-            if (player.body.onFloor()) {
-            player.play('walk', true);
-            }
-        } else {
-            // If no keys are pressed, the player keeps still
-            player.setVelocityX(0);
-            player.play('jump', true);
-            // Only show the idle animation if the player is footed
-            // If this is not included, the player would look idle while jumping
-            if (player.body.onFloor()) {
-            player.play('idle', true);
-            }
-        }
-    // }
+  // flip the player depending on the velocity of the player
+  // if (player.body.velocity.x > 0) {
+    // player.setFlipX(true)
+  // } else if (player.body.velocity.x < 0) {
+    // player.setFlipX(false)
+  // }
 
-    // Player can jump while walking any direction by pressing the space bar
-    // or the 'UP' arrow
-    if ((this.cursors.space.isDown || this.cursors.up.isDown) && player.body.onFloor() && jumps == 0 && doubleJump == false) {
-        player.setVelocityY(-620);
-        player.play('jump', true);
-        jumps += 1
-    }
+  // detect left and right movement and go at constant speedX
+  if (cursors.left.isDown) {
+    player.setVelocityX(-speedX)
+    player.play('walk', true)
+    player.setFlipX(false)
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(speedX)
+    player.setFlipX(true)
+    player.play('walk', true)
+  } else {
+    player.setVelocityX(0)
+    player.play('jump',)
     if (player.body.onFloor()) {
-        jumps = 0 
-        doubleJump = false
+      player.play('idle', true)
     }
-    
-    if (player.body.y >= 560) {
-        player.body.x = spawnX
-        player.body.y = spawnY
-    }
+  }
 
-    // flip player
-    if (player.body.velocity.x > 0) {
-        player.setFlipX(true);
-    } else if (player.body.velocity.x < 0) {
-        // otherwise, make them face the other side
-        player.setFlipX(false);
-    }
-
-    if (health == 0) {
-        player.body.x = spawnX
-        player.body.y = spawnY
-        player.clearTint()
-        health = 100
-    }
-
-    // console.log(health)
-
-}
-
-function hitEnemy(player, enemy) {
-    health -= 10
-    player.setTint(0xff0000)
-    if (player.body.x >= enemy.body.x) {
-        player.body.x += 5
-    } else {
-        player.body.x -= 5
-    }
+  if (cursors.up.isDown && player.body.onFloor()) {
+    player.body.setVelocityY(speedY)
+  }
 }
